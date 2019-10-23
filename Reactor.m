4f606fb26f1eea57cd2c353e8e7df12dc38087a6
macklo <maciek2608@gmail.com>
2019-10-23 23:44:16 +0200

    Testing differential equations

--- Reactor.m
+++ Reactor.m
@@ -38,15 +38,24 @@ classdef Reactor
 			P_0 = sqrt((2 * obj.f_p * x(2) * obj.Z_I * exp(-obj.E_I/(obj.R*obj.T))) ...
 				/(obj.Z_Td * exp(-obj.E_Td/(obj.R*obj.T)) + obj.Z_Tc * exp(-obj.E_Tc/(obj.R*obj.T))));
 			
-			dx1 = -(obj.Z_exp_ERT(obj.Z_P, obj.E_P) + obj.Z_exp_ERT(obj.Z_fm, obj.E_fm)) * x(1) * P_0 ...
+			dx1_test = -(obj.Z_exp_ERT(obj.Z_P, obj.E_P) + obj.Z_exp_ERT(obj.Z_fm, obj.E_fm)) * x(1) * P_0 ...
 				- (obj.F * x(1)) / obj.V + (obj.F * obj.C_min) / obj.V;
-
-% 			dx1 = 10 * (6 - x(1)) - 2.4568 * x(1) * sqrt(x(2));
+			
+			dx2_test = -(obj.Z_exp_ERT(obj.Z_I, obj.E_I) * x(2)) - (obj.F * x(2)) / obj.V + (u * obj.C_Iin) / obj.V;
+			
+			dx3_test = (0.5 * obj.Z_exp_ERT(obj.Z_Tc, obj.E_Tc) + obj.Z_exp_ERT(obj.Z_Td, obj.E_Td)) * P_0 * P_0 ...
+				+ obj.Z_exp_ERT(obj.Z_fm, obj.E_fm) * x(1) * P_0 - (obj.F * x(3)) / obj.V;
+			
+			dx4_test = obj.M_m * (obj.Z_exp_ERT(obj.Z_P, obj.E_P) + obj.Z_exp_ERT(obj.Z_fm, obj.E_fm)) * x(1) * P_0 ...
+				- (obj.F * x(4)) / obj.V;
+			
+			dx1 = 10 * (6 - x(1)) - 2.4568 * x(1) * sqrt(x(2));
 			dx2 = 80 * u - 10.1022 * x(2);
 			dx3 = 0.0024121 * x(1) * sqrt(x(2)) + 0.112191 - 10 *x(3);
 			dx4 = 245.978 * x(1) * sqrt(x(2)) - 10 * x(4);
 
 			dx = [dx1; dx2; dx3; dx4];
+%             disp(dx4 - dx4_test);
 		end
 		
 		function e = Z_exp_ERT(obj, Z, E)
--- test.m
+++ test.m
@@ -5,15 +5,20 @@ clc
 r = Reactor;
 
 x0 = [5.50677; 0.132906; 0.0019752; 49.3818];
+% x0 = [1 1 1 1];
 u0 = 0.016783;
-u0 = 1;
+
 y0 = x0(4)/x0(3);
 
 t0 = 0;
 tfinal = 10;
 
-[t, x] = r.simulate(x0, u0, t0, tfinal);
+figure
+hold on
+grid on
 
-y = x(:,4)./x(:, 3);
-
-plot(t, y)
+for mult = 0
+	[t, x] = r.simulate(x0, u0 + mult*u0, t0, tfinal);
+	y = x(:,4)./x(:, 3);
+	stairs(t, y)
+end
