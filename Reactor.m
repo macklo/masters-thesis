bfa3efc5a0e3924819a67241add4ba8d3b76ba83
macklo <maciek2608@gmail.com>
2019-10-28 09:38:21 +0100

    not working

--- Reactor.m
+++ Reactor.m
@@ -1,4 +1,4 @@
-classdef Reactor
+classdef Reactor < handle
 	properties
 		Z_Tc = 3.8223e10;
 		Z_Td = 3.1457e11;
@@ -23,39 +23,78 @@ classdef Reactor
 		M_m   = 100.12;
 		C_min = 6.0;
 		T     = 335;
+		
+		Ts = 0.01;
+		workpoint
+		t = [];
+		x = [];
+		y = [];
+		u = [];
+		iteration = 1;
 	end
 	
 	methods
-		function obj = Reactor()
+		function obj = Reactor(workpoint)
+			obj.workpoint = workpoint;
+			obj.x = workpoint.x0;
+			obj.t = workpoint.t0;
+			obj.y = workpoint.y0;
+			obj.u = workpoint.u0;
 		end
 		
-		function [t, x] = simulate(obj, x0, u0, t0, tfinal)
-			[t, x] = ode45(@(t, x) obj.differential(t, x, u0), [t0 tfinal], x0);
-		end
-		
-		function dx = differential(obj, t, x, u)
+		function [y, t] = simulate(obj, u)
+			obj.t = [obj.t obj.t + obj.Ts];
 			
 			P_0 = sqrt((2 * obj.f_p * x(2) * obj.Z_I * exp(-obj.E_I/(obj.R*obj.T))) ...
 				/(obj.Z_Td * exp(-obj.E_Td/(obj.R*obj.T)) + obj.Z_Tc * exp(-obj.E_Tc/(obj.R*obj.T))));
 			
-			dx1_test = -(obj.Z_exp_ERT(obj.Z_P, obj.E_P) + obj.Z_exp_ERT(obj.Z_fm, obj.E_fm)) * x(1) * P_0 ...
+			dx1 = -(obj.Z_exp_ERT(obj.Z_P, obj.E_P) + obj.Z_exp_ERT(obj.Z_fm, obj.E_fm)) * x(1) * P_0 ...
 				- (obj.F * x(1)) / obj.V + (obj.F * obj.C_min) / obj.V;
 			
-			dx2_test = -(obj.Z_exp_ERT(obj.Z_I, obj.E_I) * x(2)) - (obj.F * x(2)) / obj.V + (u * obj.C_Iin) / obj.V;
+			dx2 = -(obj.Z_exp_ERT(obj.Z_I, obj.E_I) * x(2)) - (obj.F * x(2)) / obj.V + (u * obj.C_Iin) / obj.V;
 			
-			dx3_test = (0.5 * obj.Z_exp_ERT(obj.Z_Tc, obj.E_Tc) + obj.Z_exp_ERT(obj.Z_Td, obj.E_Td)) * P_0 * P_0 ...
+			dx3 = (0.5 * obj.Z_exp_ERT(obj.Z_Tc, obj.E_Tc) + obj.Z_exp_ERT(obj.Z_Td, obj.E_Td)) * P_0 * P_0 ...
 				+ obj.Z_exp_ERT(obj.Z_fm, obj.E_fm) * x(1) * P_0 - (obj.F * x(3)) / obj.V;
 			
-			dx4_test = obj.M_m * (obj.Z_exp_ERT(obj.Z_P, obj.E_P) + obj.Z_exp_ERT(obj.Z_fm, obj.E_fm)) * x(1) * P_0 ...
+			dx4 = obj.M_m * (obj.Z_exp_ERT(obj.Z_P, obj.E_P) + obj.Z_exp_ERT(obj.Z_fm, obj.E_fm)) * x(1) * P_0 ...
 				- (obj.F * x(4)) / obj.V;
 			
-			dx1 = 10 * (6 - x(1)) - 2.4568 * x(1) * sqrt(x(2));
-			dx2 = 80 * u - 10.1022 * x(2);
-			dx3 = 0.0024121 * x(1) * sqrt(x(2)) + 0.112191 - 10 *x(3);
-			dx4 = 245.978 * x(1) * sqrt(x(2)) - 10 * x(4);
+			x = obj.x(obj.iteration, 1);
+			
+			dx = obj.differential(x, u)
+			
+			k1 = obj.Ts * obj.differential(x, u);
+			k2 = obj.Ts * obj.differential(
+			
+			x1 = obj.x(obj.iteration, 1) + Ts (obj.Ts * dx1
+		end
+		
+% 		function [t, x] = simulate(obj, x0, u0, t0, tfinal)
+% 			[t, x] = ode45(@(t, x) obj.differential(t, x, u0), t0:obj.Ts:tfinal, x0);
+% 		end
+		
+		function dx = differential(obj, ~, x, u)
+			
+			P_0 = sqrt((2 * obj.f_p * x(2) * obj.Z_I * exp(-obj.E_I/(obj.R*obj.T))) ...
+				/(obj.Z_Td * exp(-obj.E_Td/(obj.R*obj.T)) + obj.Z_Tc * exp(-obj.E_Tc/(obj.R*obj.T))));
+			
+			dx1 = -(obj.Z_exp_ERT(obj.Z_P, obj.E_P) + obj.Z_exp_ERT(obj.Z_fm, obj.E_fm)) * x(1) * P_0 ...
+				- (obj.F * x(1)) / obj.V + (obj.F * obj.C_min) / obj.V;
+			
+			dx2 = -(obj.Z_exp_ERT(obj.Z_I, obj.E_I) * x(2)) - (obj.F * x(2)) / obj.V + (u * obj.C_Iin) / obj.V;
+			
+			dx3 = (0.5 * obj.Z_exp_ERT(obj.Z_Tc, obj.E_Tc) + obj.Z_exp_ERT(obj.Z_Td, obj.E_Td)) * P_0 * P_0 ...
+				+ obj.Z_exp_ERT(obj.Z_fm, obj.E_fm) * x(1) * P_0 - (obj.F * x(3)) / obj.V;
+			
+			dx4 = obj.M_m * (obj.Z_exp_ERT(obj.Z_P, obj.E_P) + obj.Z_exp_ERT(obj.Z_fm, obj.E_fm)) * x(1) * P_0 ...
+				- (obj.F * x(4)) / obj.V;
+			
+% 			dx1 = 10 * (6 - x(1)) - 2.4568 * x(1) * sqrt(x(2));
+% 			dx2 = 80 * u - 10.1022 * x(2);
+% 			dx3 = 0.0024121 * x(1) * sqrt(x(2)) + 0.112191 - 10 *x(3);
+% 			dx4 = 245.978 * x(1) * sqrt(x(2)) - 10 * x(4);
 
 			dx = [dx1; dx2; dx3; dx4];
-%             disp(dx4 - dx4_test);
 		end
 		
 		function e = Z_exp_ERT(obj, Z, E)
--- test.m
+++ test.m
@@ -2,23 +2,28 @@ close all
 clear
 clc
 
-r = Reactor;
-
 x0 = [5.50677; 0.132906; 0.0019752; 49.3818];
 % x0 = [1 1 1 1];
 u0 = 0.016783;
 
 y0 = x0(4)/x0(3);
 
+workpoint = struct('x0', x0, 'u0', u0, 'y0', y0, 't0', 0);
+
 t0 = 0;
-tfinal = 10;
+tfinal = 5;
+
+r = Reactor(workpoint);
 
 figure
 hold on
 grid on
+legends = [];
 
-for mult = 0
-	[t, x] = r.simulate(x0, u0 + mult*u0, t0, tfinal);
+for mult = -0.8:0.1:1
+	u = u0 + mult * u0;
+	legends = [legends u];
+	[t, x] = r.simulate(x0, u, t0, tfinal);
 	y = x(:,4)./x(:, 3);
 	stairs(t, y)
 end
